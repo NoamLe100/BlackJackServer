8ab46168ca7d8d0b1b52f5c7733159c9fc8021e9
NoamLe100 <noamle132@gmail.com>
2026-07-09 11:19:37 +0300

    Initial commit - basic express server setup

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express ,{ Request ,Response} from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req: Request, res: Response) => 
+{
+  res.send('Hello World!');
+});
+
+
+app.listen(port, () => 
+{
+  console.log(`Example app listening on port ${port}`);
+});
