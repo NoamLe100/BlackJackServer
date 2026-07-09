91c845592b01664ee024f6472f0331e9df3caa7a
NoamLe100 <noamle132@gmail.com>
2026-07-09 13:31:46 +0300

    Add Deck class with card generation and drawCard method

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express ,{ Request ,Response} from 'express';
 import cors from 'cors';
+import { Deck } from './models/deck';
 
 const app = express();
 const port = 3000;
+const myDeck = new Deck();
 
 app.use(cors());
 app.use(express.json());
@@ -16,5 +18,20 @@ app.get('/', (req: Request, res: Response) =>
 
 app.listen(port, () => 
 {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
+
+app.get('star', (req: Request, res: Response) => 
+{
+    console.log(myDeck.getCardsRemaining());
+});
+
+app.get('star', (req: Request, res: Response) => 
+{
+    const card = myDeck.drawCard();
+    if (card) {
+        console.log(card.suit); 
+}   
+});
+
+
---
+++ src/models/deck.ts
@@ -0,0 +1,33 @@
+import { Card, Suit, CardValue } from './card';
+
+const suits: Suit[] = ['hearts', 'diamonds', 'clubs', 'spades'];
+const values: CardValue[] = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+export class Deck {
+  private cards: Card[];
+
+ constructor() {
+  this.cards = [];
+
+  for (const suit of suits) {
+    for (const value of values) {
+      this.cards.push({
+        suit: suit,
+        value: value,
+        imagePath: `${value}_of_${suit}.png`
+      });
+    }
+  }
+}
+  public getCardsRemaining(): number {
+    return this.cards.length;
+  }
+
+  public drawCard()
+  {
+    return this.cards.pop();
+  }
+}
+
+  
+   
