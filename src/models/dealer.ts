f3c8798f96801490f05aca9998029c1f390eda7a
NoamLe100 <noamle132@gmail.com>
2026-07-13 13:55:25 +0300

    Add Player class with money, bet, cards, and sum tracking

---
+++ src/models/dealer.ts
@@ -0,0 +1,49 @@
+import { Card, CardValue } from './card';
+import { Deck } from './deck';
+
+const valueMap: Record<CardValue, number> = {
+  'A': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+  'J': 10, 'Q': 10, 'K': 10
+};
+
+export class Dealer {
+  private cards: Card[];
+  private sum: number;
+
+  constructor(deck: Deck) {
+    this.cards = [];
+    this.cards.push(deck.drawCard());
+    this.cards.push(deck.drawCard());
+    this.sum = 0;
+  }
+
+  public addCard(card: Card): void {
+    this.cards.push(card);
+  }
+
+  public calculateSum(): number {
+    let sum = 0;
+    let aceCount = 0;
+
+    for (const card of this.cards) {
+      sum += valueMap[card.value];
+      if (card.value === 'A') aceCount++;
+    }
+
+    while (sum > 21 && aceCount > 0) {
+      sum -= 10;
+      aceCount--;
+    }
+
+    this.sum = sum;
+    return sum;
+  }
+
+  public shouldHit(): boolean {
+    return this.calculateSum() < 17;
+  }
+
+  public getCards(): Card[] {
+    return this.cards;
+  }
+}
